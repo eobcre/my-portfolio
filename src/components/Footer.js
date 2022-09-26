b2ae1032871191d21b0b9453dc0153a7c0f29997
eobcre <eobcre@gmail.com>
2022-09-25 21:21:30 -0400

    Fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="flex justify-between items-center px-8 h-16 bg-dark-blue text-white">
-      <p>© Emmy Manning 2023</p>
+    <div className="flex justify-center items-center px-8 h-16 bg-dark-blue text-white">
+      <p className="">© Emmy Manning 2023</p>
     </div>
   );
 };
