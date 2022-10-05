ae1bc814d68b12a0546881c85fd97b76502c6fca
eobcre <eobcre@gmail.com>
2022-10-05 06:13:38 -0400

    Logo fixed

--- src/components/AppProjects.js
+++ src/components/AppProjects.js
@@ -1,61 +1,30 @@
 import React from "react";
-// import todoLogo from "../img/todo.jpeg";
+import todoLogo from "../img/todo-logo.svg";
+import caitzLogo from "../img/caitz-logo.png";
 
 const AppProjects = (props) => {
   return (
     <div className="h-auto bg-dark-blue text-white pb-10">
-      <main className="pl-5">
-        <article className="mx-auto w-4/5 md:w-3/5">
-          {/* Title */}
-          <h1 className="text-4xl font-bold tracking-wide py-5 md:5xl">
-            {props.title}
-          </h1>
-          {/* Contents */}
-          <div className="mx-auto text-md md:text-xl">
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              <br />
-              Quae harum laudantium inventore iusto quod,
-              <br />
-              sint architecto molestiae nisi quibusdam possimus deleniti itaque
-              magni mollitia accusamus molestias totam tempora ducimus quam?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              <br />
-              Quae harum laudantium inventore iusto quod,
-              <br />
-              sint architecto molestiae nisi quibusdam possimus deleniti itaque
-              magni mollitia accusamus molestias totam tempora ducimus quam?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              <br />
-              Quae harum laudantium inventore iusto quod,
-              <br />
-              sint architecto molestiae nisi quibusdam possimus deleniti itaque
-              magni mollitia accusamus molestias totam tempora ducimus quam?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              <br />
-              Quae harum laudantium inventore iusto quod,
-              <br />
-              sint architecto molestiae nisi quibusdam possimus deleniti itaque
-              magni mollitia accusamus molestias totam tempora ducimus quam?
-            </p>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              <br />
-              Quae harum laudantium inventore iusto quod,
-              <br />
-              sint architecto molestiae nisi quibusdam possimus deleniti itaque
-              magni mollitia accusamus molestias totam tempora ducimus quam?
-            </p>
-          </div>
-        </article>
-        {/* <img src={todoLogo} alt="Logo" /> */}
-      </main>
+      <div className="mx-auto w-4/5 md:w-3/5">
+        {/* Title */}
+        <h1 className="text-4xl font-bold tracking-wide py-10 md:5xl">
+          {props.title}
+        </h1>
+        {/* Contents */}
+        <div className="grid grid-cols-1 justify-items-center items-center gap-10 md:grid md:grid-cols-3">
+          <img
+            src={todoLogo}
+            alt="Logo"
+            className="transition-transform durtion-200 hover:scale-110"
+          />
+          <img
+            src={caitzLogo}
+            alt="Logo"
+            width="200"
+            className="transition-transform duration-200 hover:scale-110"
+          />
+        </div>
+      </div>
     </div>
   );
 };
